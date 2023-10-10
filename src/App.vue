9f40b6b745bc716e45736a68c5d2bcdbeaf1b587
peppegerace <peppegerace47@gmail.com>
2023-10-10 16:13:30 +0200

    [fix] syntatix error in script

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup>
+<script>
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
 import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+}
 
 </script>
 
